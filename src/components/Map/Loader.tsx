8e9aa67345cefcdd0172d30e28a75637b1cde6fc
norbigigakoks <norbeegsoft@gmail.com>
2024-10-16 01:51:46 +0200

    Dodano .npmrc

--- src/components/Map/Loader.tsx
+++ src/components/Map/Loader.tsx
@@ -1,6 +1,8 @@
 import {RotatingLines} from 'react-loader-spinner';
+import React from "react";
 
 const Loader: React.FC = () => {
+
     return (
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <RotatingLines
