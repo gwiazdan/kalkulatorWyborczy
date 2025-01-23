92e17f150db6d333a065a9126a8902b4fc0b8ce8
Norbert Gwiazda <norbert.gwiazda@icloud.com>
2025-01-23 21:10:43 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import tailwindcss from 'tailwindcss';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  esbuild: {
-    target: 'esnext',
+  build: {
+  target: 'esnext',
   },
   css: {
     postcss: {
